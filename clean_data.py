a6decaa83af1c3b8f4623e70947dfa951b24f40f
MichaelJulian <mikeyjmikeyj@gmail.com>
2016-06-17 09:34:54 -0700

    clean data and output csv

--- clean_data.py
+++ clean_data.py
@@ -1,32 +1,53 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # Load Data
 def load_data():
-    census = pd.read_excel('data/SantaClaraCountyHomelessCensus.xlsx', sheetname=0)
+    census = pd.read_excel('data/SantaClaraCountyHomelessCensus.xlsx',
+                           sheetname=0)
     return census
 
 def load_variables():
-    variable_labels = pd.read_excel('data/SantaClaraCountyHomelessCensus.xlsx', sheetname=1, skiprows=3)
-    variable_labels.ix[variable_labels['LABEL'] == '<ninguno>', 'LABEL'] = variable_labels.ix[variable_labels['LABEL'] == '<ninguno>', 'Variable']
-    variable_labels.columns = ['Variable', 'Label']
+    variable_labels = pd.read_excel('data/SantaClaraCountyHomelessCensus.xlsx',
+                                    sheetname=1, skiprows=3)
+    variable_labels.ix[variable_labels['LABEL'] == '<ninguno>', 'LABEL'] = \
+        variable_labels.ix[variable_labels['LABEL'] == '<ninguno>', 'Variable']
+    variable_labels.columns = ['Variable', 'Label', 'New Label']
     return variable_labels
 
 def load_values():
-    value_labels = pd.read_excel('data/SantaClaraCountyHomelessCensus.xlsx', sheetname=2, skiprows=2, skipfooter=1)
+    value_labels = pd.read_excel('data/SantaClaraCountyHomelessCensus.xlsx',
+                                 sheetname=2, skiprows=2, skipfooter=1)
     value_labels = value_labels.ix[:, 1:]
     value_labels.columns = ['Variable', 'Value', 'Label']
-    for ix, variable in enumerate(value_labels['Variable']):
-        if type(variable) != float:
-            variable_name = variable
-        else:
-            value_labels['Variable'][ix] = variable_name
+    value_labels['Variable'] = value_labels['Variable'].fillna(method='ffill')
     return value_labels
 
-def clean_data(census, variables, values):
-    for ix, row in values.iterrows():
-        val = row['Value']
+def fill_NAs(census):
+    # Fill NAs with 0s
+    for col in census.columns[7:]:
+        census[col] = census[col].fillna('No Answer')
+        census[col] = census[col].astype(str)
+    return census
+
+def replace_answers(census, variables, values):
+    # Replace Data Cells with Intelligible Strings
+    for ix, row in values[25:].iterrows():
+        val = str(float(row['Value']))
         var = row['Variable']
         lab = row['Label']
-        census.ix[census[var].astype(int).astype(str) == val, var] = lab
+        census[var] = census[var].replace(val, lab)
+    return census
+
+def replace_columns(census, variables, values):
+    census.columns = variables['New Label'].values
+    return census
+
+if __name__ == '__main__':
+    census, variables, values = load_data(), load_variables(), load_values()
+    census = fill_NAs(census)
+    census = replace_answers(census, variables, values)
+    census = replace_columns(census, variables, values)
+    census.to_csv('scch_census.csv', index=False)
